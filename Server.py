577a4cba4ca5783c78abeffc3dbe3438249588fe
SurawutSukkum <74821839+SurawutSukkum@users.noreply.github.com>
2022-05-16 17:07:47 +0700

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,8 @@
+import socket
+HOST_socket = '10.144.241.230'  
+PORT_socket = 5432
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST_socket, PORT_socket))
+s.sendall(b'Hello World')
+data = s.recv(1024)
+print('received:', repr(data))
