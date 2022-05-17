0a13666cfc5bbb552f2c917b20705dc2d4335e3d
SurawutSukkum <74821839+SurawutSukkum@users.noreply.github.com>
2022-05-17 10:01:21 +0700

    Update Client.py

--- Client.py
+++ Client.py
@@ -4,8 +4,8 @@ import paho.mqtt.subscribe as subscribe
 import socket
 import time
 
-HOST_socket = ''xx.xx.xx.xx''  
-PORT_socket = 5432         
+HOST_socket = '10.144.241.230'
+PORT_socket = 5432
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST_socket, PORT_socket))
 s.listen()
@@ -13,19 +13,22 @@ s.listen()
 hostname = "10.144.241.230"
 port = 1883
 auth = {
- 'username':' ',
- 'password':' '
+    'username': ' ',
+    'password': ' '
 }
 print("waiting for connection")
 while True:
- connection, client_address = s.accept() 
- data = connection.recv(1024)
- if data:
-   print("sending data back to the client")
-   msg = subscribe.simple("data",  hostname=hostname, port=port, auth=auth)
-   print("%s %s" % (msg.topic, msg.payload))
-   connection.sendall(data)
+    connection, client_address = s.accept()
+    data = connection.recv(1024)
+    if data:
+        print("sending data back to the client")
+        msg = subscribe.simple("data", hostname=hostname, port=port, auth=auth)
+        msg1 = subscribe.simple("time", hostname=hostname, port=port, auth=auth)
+        msg2 = subscribe.simple("status_data", hostname=hostname, port=port, auth=auth)
+        print("%s %s %s %s %s %s" % (msg.topic, msg.payload,msg1.topic, msg1.payload,msg2.topic, msg2.payload))
+        connection.sendall( msg.payload)
+        connection.sendall( msg1.payload)
+        connection.sendall( msg2.payload)
 
-  
 
 
